c3de84aad29694c7a51e75fcb3e08d854ad91bcf
Nikolai Nanev <niki4etooo@gmail.com>
2021-01-30 13:23:28 +0200

    Abstract mouse, keyboard and callbacks

--- MultipleLightSources/main.cpp
+++ MultipleLightSources/main.cpp
@@ -1,40 +1,38 @@
 
 
-#include "glad/glad.h"
+#include "include/glad/glad.h"
 #include <GLFW/glfw3.h>
 
-#include "glm/glm.hpp"
-#include "glm/gtc/matrix_transform.hpp"
-#include "glm/gtc/type_ptr.hpp"
+#include "include/glm/glm.hpp"
+#include "include/glm/gtc/matrix_transform.hpp"
+#include "include/glm/gtc/type_ptr.hpp"
 
-#include "shader.h"
-#include "camera.h"
+#include "include/shader.h"
+#include "include/camera.h"
 
 #include <iostream>
 
-#include "loaders/texture_loader.h"
+#include "include/loaders/texture_loader.h"
+#include "include/callbacks.h"
+#include "include/input_processing.h"
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void mouse_callback(GLFWwindow* window, double xpos, double ypos);
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
-void processInput(GLFWwindow *window);
 void lightParameters(Shader shader, glm::vec3 pointLightPositions[]);
+
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
-
-// camera
-Camera camera(glm::vec3(0.0, 0.0f, 3.0f));
-float lastX = SCR_WIDTH / 2.0f;
-float lastY = SCR_HEIGHT / 2.0f;
-bool firstMouse = true;
-
 // timing
 float deltaTime = 0.0f;	
 float lastFrame = 0.0f;
 
+bool firstMouse = true;
+
 // lighting
 glm::vec3 lightPos(0.0f, 0.0f, 0.0f);
+
+Callbacks *callback = new Callbacks();
+InputProcessing *input = new InputProcessing();
+
 int main()
 {
     // glfw: initialize and configure
@@ -58,9 +56,9 @@ int main()
         return -1;
     }
     glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    glfwSetCursorPosCallback(window, mouse_callback);
-    glfwSetScrollCallback(window, scroll_callback);
+    glfwSetFramebufferSizeCallback(window, callback->framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, callback->mouse_callback);
+    glfwSetScrollCallback(window, callback->scroll_callback);
 
     // tell GLFW to capture our mouse
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
@@ -205,7 +203,7 @@ int main()
 
         // input
         // -----
-        processInput(window);
+        input->keyboard_input(window, deltaTime);
 
         // render
         // ------
@@ -214,14 +212,14 @@ int main()
 		
 		// be sure to activate shader when setting uniforms/drawing objects
         lightingShader.use();
-        lightingShader.setVec3("viewPos", camera.Position);
+        lightingShader.setVec3("viewPos", camera->Position);
         lightingShader.setFloat("material.shininess", 32.0f);
 		
         lightParameters(lightingShader, pointLightPositions);
 		
         // view/projection transformations
-        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        glm::mat4 view = camera.GetViewMatrix();
+        glm::mat4 projection = glm::perspective(glm::radians(mouse.fov), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 view = camera->GetViewMatrix();
         lightingShader.setMat4("projection", projection);
         lightingShader.setMat4("view", view);
 		
@@ -284,66 +282,6 @@ int main()
     return 0;
 }
 
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        camera.ProcessKeyboard(FORWARD, deltaTime*1.5f);
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        camera.ProcessKeyboard(BACKWARD, deltaTime*1.5f);
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        camera.ProcessKeyboard(LEFT, deltaTime*1.5f);
-    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        camera.ProcessKeyboard(RIGHT, deltaTime*1.5f);
-}
-
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
-    glViewport(0, 0, width, height);
-}
-
-
-// glfw: whenever the mouse moves, this callback is called
-// -------------------------------------------------------
-void mouse_callback(GLFWwindow* window, double xpos, double ypos)
-{
-    if (firstMouse)
-    {
-        lastX = xpos;
-        lastY = ypos;
-        firstMouse = false;
-    }
-
-    float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
-
-    lastX = xpos;
-    lastY = ypos;
-
-    camera.ProcessMouseMovement(xoffset, yoffset);
-}
-
-// glfw: whenever the mouse scroll wheel scrolls, this callback is called
-// ----------------------------------------------------------------------
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
-{
-    camera.ProcessMouseScroll(yoffset);
-}
-
-// Gold material properties
-//lightingShader.setVec3("material.ambient", 0.24725f, 0.1995f, 0.0745f);
-//lightingShader.setVec3("material.diffuse", 0.75164f, 0.60648f, 0.22648f);
-//lightingShader.setVec3("material.specular", 0.628281f, 0.555802f, 0.366065f);
-//lightingShader.setFloat("material.shininess", 32.0f*0.4f);
-
 void lightParameters(Shader shader, glm::vec3 pointLightPositions[])
 {
 		/*
@@ -390,8 +328,8 @@ void lightParameters(Shader shader, glm::vec3 pointLightPositions[])
         shader.setFloat("pointLights[3].linear", 0.09);
         shader.setFloat("pointLights[3].quadratic", 0.032);
         // spotLight
-        shader.setVec3("spotLight.position", camera.Position);
-        shader.setVec3("spotLight.direction", camera.Front);
+        shader.setVec3("spotLight.position", camera->Position);
+        shader.setVec3("spotLight.direction", camera->Front);
         shader.setVec3("spotLight.ambient", 0.0f, 0.0f, 0.0f);
         shader.setVec3("spotLight.diffuse", 1.0f, 1.0f, 1.0f);
         shader.setVec3("spotLight.specular", 1.0f, 0.3f, 1.0f);
