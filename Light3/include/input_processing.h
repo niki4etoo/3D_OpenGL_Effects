63ffc8fb382460e396dc75775543bb55b9e22d56
Nikolai Nanev <niki4etooo@gmail.com>
2021-02-21 07:32:07 +0200

    added code style format (llvm)

--- Light3/include/input_processing.h
+++ Light3/include/input_processing.h
@@ -1,46 +1,46 @@
-#ifndef _INPUT_PROCESSING_H
-#define _INPUT_PROCESSING_H
+#ifndef INPUT_PROCESSING_H
+#define INPUT_PROCESSING_H
 
 #include "camera.h"
 
 class InputProcessing {
 	bool blinn = false;
 	bool blinnKeyPressed = false;
-public:
-	InputProcessing(){}
-	~InputProcessing(){}
-	std::string setBlinn(std::string text)
-	{
-		return text;
-	}
-	
-	bool getBlinn()
-	{
-		return this->blinn;
-	}
-	
-	void keyboard_input(GLFWwindow *window, float deltaTime) {		
+
+       public:
+	InputProcessing() {}
+	~InputProcessing() {}
+	std::string setBlinn(std::string text) { return text; }
+
+	bool getBlinn() { return this->blinn; }
+
+	void keyboard_input(GLFWwindow *window, float deltaTime) {
 		camera->MovementSpeed = 5.5f * deltaTime;
 		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 			glfwSetWindowShouldClose(window, true);
-			
-		//Camera controls
+
+		// Camera controls
 		if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-			camera->Position += camera->MovementSpeed * camera->Front;
+			camera->Position +=
+			    camera->MovementSpeed * camera->Front;
 		if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-			camera->Position -= camera->MovementSpeed * camera->Front;
+			camera->Position -=
+			    camera->MovementSpeed * camera->Front;
 		if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-			camera->Position -= glm::normalize(glm::cross(camera->Front, camera->Up)) * camera->MovementSpeed;
+			camera->Position -= glm::normalize(glm::cross(
+						camera->Front, camera->Up)) *
+					    camera->MovementSpeed;
 		if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-			camera->Position += glm::normalize(glm::cross(camera->Front, camera->Up)) * camera->MovementSpeed;
-			
-		if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS && !blinnKeyPressed) 
-		{
+			camera->Position += glm::normalize(glm::cross(
+						camera->Front, camera->Up)) *
+					    camera->MovementSpeed;
+
+		if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS &&
+		    !blinnKeyPressed) {
 			blinn = !blinn;
 			blinnKeyPressed = true;
 		}
-		if (glfwGetKey(window, GLFW_KEY_B) == GLFW_RELEASE) 
-		{
+		if (glfwGetKey(window, GLFW_KEY_B) == GLFW_RELEASE) {
 			blinnKeyPressed = false;
 		}
 	}
